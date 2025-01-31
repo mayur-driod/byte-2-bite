7df85f8580cd40b9571c8a6263b3ae4efa624809
Sup-ri-tha <supritharajashekar10@gmail.com>
2025-01-31 22:21:22 +0530

    Added Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import './Navbar.css';
 
 
 function NavBar() {
   return (
     <div className="topnav">
       <link href="https://fonts.googleapis.com/css2?family=Sixtyfour+Convergence&display=swap" rel="stylesheet"></link>
-      <nav>
-        <div id='mks'><h2>Mayur</h2></div>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/About">About Me</NavLink>
-        <NavLink to="/Projects">Projects</NavLink>
-        <NavLink to="/Experience">Experience</NavLink>
-        <NavLink to="/Intrests">Intrests</NavLink>
-        <NavLink to="/Education">Education</NavLink>
-        <NavLink to="/Contact">Contact</NavLink>
+
+      {/* Top Navigation (Dashboard on the left, Profile on the right) */}
+      <nav className="nav-links">
+        <NavLink to="/Dashboard"><img src="https://cdn-icons-png.flaticon.com/128/6015/6015685.png" alt="" height={25} width={25}/></NavLink>
+        <NavLink to="/Profile"><img src="https://cdn-icons-png.flaticon.com/128/1077/1077114.png" alt="" height={25} width={25}  /></NavLink>      
+      </nav>
+
+      {/* Bottom-left Navigation (About Us & Contact) */}
+      <nav className="bottom-links">
+        <NavLink to="/About">AboutUs </NavLink>
+        <NavLink to="/Contact">Contact </NavLink>
+        <NavLink to="/FAQ">FAQs</NavLink>
       </nav>
     </div>
   );
